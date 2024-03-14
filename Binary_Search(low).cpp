566431d5c4a6d0bb5adbc9f6e93dde67524d044a
n3wt0n._th <140224520+newtonabc@users.noreply.github.com>
2024-03-14 14:59:10 +0700

    Update Binary_Search(low).cpp

--- Binary_Search(low).cpp
+++ Binary_Search(low).cpp
@@ -5,9 +5,9 @@ int main(){
 	int fd=2;
 	int l=0,r=11;
 	while(l<r){
-		int mid=(l+r+1)/2;
-		if(fd>=mid) l=mid;
-		else r=mid-1;
+		int mid=(l+r)/2;
+		if(fd>arr[mid]) l=mid+1;
+		else r=mid;
 	}
 	cout<<l;
 }
