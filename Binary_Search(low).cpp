0c06de303c2f8df014d196e232b229d573ca404c
n3wt0n._th <140224520+newtonabc@users.noreply.github.com>
2024-03-14 15:31:58 +0700

    Update Binary_Search(low).cpp

--- Binary_Search(low).cpp
+++ Binary_Search(low).cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
 	int arr[]={1,1,2,2,2,3,4,4,4,4,5,6};
 	int fd=2;
-	int l=0,r=11;
+	int l=0,r=11,mid;
 	while(l<r){
-		int mid=(l+r)/2;
-		if(fd>arr[mid]) l=mid+1;
-		else r=mid;
+		mid=(l+r)/2;
+		if(arr[mid]>=fd) r=mid;
+		else l=mid+1;
 	}
 	cout<<l;
 }
