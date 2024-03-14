dcfb9ae27ef4566a47e4590e48b544847e4cf300
n3wt0n._th <140224520+newtonabc@users.noreply.github.com>
2024-03-14 15:32:53 +0700

    Create Binary_Search(up).cpp

---
+++ Binary_Search(up).cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int arr[]={1,1,2,2,2,3,4,4,4,4,5,6};
+	int fd=2;
+	int l=0,r=11,mid;
+	while(l<r){
+		mid=(l+r)/2;
+		if(arr[mid]>fd) r=mid;
+		else l=mid+1;
+	}
+	cout<<l;
+}
