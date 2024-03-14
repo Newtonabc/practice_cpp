9ffa19ac9605a76abdd40f3fd0302c67d8765fb2
n3wt0n._th <140224520+newtonabc@users.noreply.github.com>
2024-03-14 15:42:55 +0700

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3+10;
+vector<int> adj[N];
+bool visited[N];
+void dfs(int u){
+	if(visited[u]) return;
+	visited[u]=true;
+	for(int i=0;i<adj[u].size();i++){
+		int v=adj[u][i];
+		dfs(v);
+	}
+}
+int main(){
+	int n,m;
+	cin>>n >>m;
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a >>b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+}
