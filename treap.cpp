168bb5304f03f4eb12f94700faa3b8dc32a36480
n3wt0n._th <140224520+newtonabc@users.noreply.github.com>
2025-01-16 14:57:13 +0700

    Create treap.cpp

---
+++ treap.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int val,pri;
+    node *l,*r;
+};
+void split(node *t,node *&l,node *&r,int k){
+    if(!t){
+        l=r=NULL;
+        return;
+    }
+    if(t->val<=k) split(t->r,t->r,r,k),l=t;
+    else split(t->l,l,t->l,k),r=t;
+}
+void merge(node *&t,node *l,node *r){
+    if(!l) return void(t=r);
+    if(!r) return void(t=l);
+    if(l->pri>r->pri) merge(l->r,l->r,r),t=l;
+    else merge(r->l,l,r->l),t=r;
+}
+void insert(node *&t,node *it){
+    if(!t) return void(t=it);
+    if(it->pri>t->pri) split(t,it->l,it->r,it->val),t=it;
+    else if(it->val<=t->val) insert(t->l,it);
+    else insert(t->r,it);
+}
+void erase(node *&t,int k){
+    if(!t) return;
+    if(t->val==k){
+        node *tmp=t;
+        merge(t,t->l,t->r);
+        free(tmp);
+    }
+    else if(k<=t->val) erase(t->l,k);
+    else erase(t->r,k);
+}
+int main(){
+    srand(time(NULL));
+}
